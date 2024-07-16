cfa6d5bac56d0100e13e0e01d8e05c2fd8f0fda4
borko <boris.papic96@gmail.com>
2024-07-16 02:33:52 +0200

    add headers and other fns

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,5 +1,11 @@
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
 def load_markdown(md_paths):
     '''Preferably use with glob,glob and get paths'''
     docs = ''
@@ -8,3 +14,8 @@ def load_markdown(md_paths):
         data = loader.load()
         docs += data[0].page_content
     return docs
+
+def interpret_markdown(docs):
+    markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+    md_header_splits = markdown_splitter.split_text(docs)
+    return md_header_splits
